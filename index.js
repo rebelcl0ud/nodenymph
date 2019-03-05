3884cf34c519ed47f91565e158b3fab7656526fd
rebelcl0ud <jovanka.placeres@gmail.com>
2019-03-04 16:07:08 -0800

    add: public & images folder; serve static files; test image served on /who route

--- index.js
+++ index.js
@@ -2,8 +2,20 @@ const express = require('express');
 
 const app = express();
 
+// serves static files, such as images, from within 'public' folder
+app.use(express.static('public'));
+
 app.get('/', (req, res) => {
 	res.send('hebwo... this is just a test... *beep*');
+})
+
+app.get('/who', (req, res) => {
+	res.send(`
+		<p>you just got routed to who page</p>
+		<div>
+			<img src='/images/spidergwen.jpeg'/>
+		</div>
+	`)
 })
 
 app.listen(3000, () => {
